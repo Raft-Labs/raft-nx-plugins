adb82c5fc86ab97667c466da4b7322585392ae90
Amal Chandran <amalchandrandev@gmail.com>
2021-10-01 15:59:01 +0530

    fix(fluentui-auth): corrected e2e test cases

--- apps/fluentui-auth-e2e/tests/fluentui-auth.spec.ts
+++ apps/fluentui-auth-e2e/tests/fluentui-auth.spec.ts
@@ -2,7 +2,7 @@ import { ensureNxProject, uniq } from '@nrwl/nx-plugin/testing';
 describe('fluentui-auth e2e', () => {
   it('should create fluentui-auth', async () => {
     const plugin = uniq('fluentui-auth');
-    ensureNxProject('@raftlabs/fluentui-auth', 'dist/libs/fluentui-auth');
+    ensureNxProject('@raftlabs/fluentui-auth', 'dist/packages/fluentui-auth');
     expect('Executor ran').toContain('Executor ran');
   }, 120000);
 });
