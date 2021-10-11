5ee32df26b3746387e5f83105a6f2b2b218f1483
Aravind Jaimon <email@aravindjaimon.me>
2021-10-11 11:54:39 +0530

    feat(frontend): added hasura providers

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,17 +1,23 @@
+import { GraphQLProvider } from '@raftlabs/hasura-react';
+import { AuthProvider } from '@raftlabs/hbp-react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { HASURA_BASE_URL } from '../configs';
+import { auth } from '../helpers/hbp-helper';
 import './styles.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Head>
-        <title>Frontend</title>
-      </Head>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <AuthProvider auth={auth}>
+      <GraphQLProvider auth={auth} url={HASURA_BASE_URL}>
+        <Head>
+          <title>Frontend</title>
+        </Head>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </GraphQLProvider>
+    </AuthProvider>
   );
 };
 
