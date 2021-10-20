bc619df39fe020832775e1f733144392d63944e1
Aravind Jaimon <email@aravindjaimon.me>
2021-10-20 13:23:11 +0530

    updated document.tsx for fluent ui

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -1,34 +1,35 @@
-/* eslint-disable react/display-name */
+/* eslint-disable react/no-danger */
 import { InjectionMode, resetIds, Stylesheet } from '@fluentui/react';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext } from 'next/document';
 import * as React from 'react';
+
+// Do this in file scope to initialize the stylesheet before Fluent UI React components are imported.
 const stylesheet = Stylesheet.getInstance();
+
+// Set the config.
 stylesheet.setConfig({
   injectionMode: InjectionMode.none,
   namespace: 'server',
 });
-export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+
+// Now set up the document, and just reset the stylesheet.
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     stylesheet.reset();
     resetIds();
-    const page = renderPage((App) => (props) => <App {...props} />);
-    return { ...page, styleTags: stylesheet.getRules(true) };
-  }
-  render() {
-    return (
-      <Html>
-        <Head>
-          <style
-            type="text/css"
-            // @ts-expect-error
-            dangerouslySetInnerHTML={{ __html: this.props.styleTags }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="fluentui-css"
+          dangerouslySetInnerHTML={{ __html: stylesheet.getRules(true) }}
+        />,
+      ],
+    };
   }
 }
+
+export default MyDocument;
