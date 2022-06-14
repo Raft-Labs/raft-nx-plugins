db59065e26c5abfc671f2ca4030d52f94251509d
Amal Chandran <amalchandrandev@gmail.com>
2022-06-14 18:09:35 +0530

    feat(hbp-react): exposed error handler + headers

--- packages/hbp-react/src/GraphQLProvider.tsx
+++ packages/hbp-react/src/GraphQLProvider.tsx
@@ -1,48 +1,80 @@
 import { Auth } from '@raftlabs/hbp-sdk';
-import { Client as WSClient, createClient as createWSClient } from 'graphql-ws';
 import React, { useEffect, useRef, useState } from 'react';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 import {
   Client,
   createClient,
   defaultExchanges,
+  errorExchange,
   Provider,
   subscriptionExchange,
 } from 'urql';
 import { useAuth } from './hooks/use-auth.hook';
 
-const createGQLClient = (url: string, auth: Auth) => {
-  const subscriptionClient = createWSClient({
-    url: url.replace('http://', 'ws://').replace('https://', 'wss://'),
-    connectionParams: () => {
-      const token = auth.getJWTToken();
+const createGQLClient = (
+  url: string,
+  auth: Auth,
+  errorHandler,
+  additionalHeaders = {}
+) => {
+  const subscriptionClient = (process as any).browser
+    ? new SubscriptionClient(
+        url.replace('http://', 'ws://').replace('https://', 'wss://'),
+        {
+          connectionParams: () => {
+            const token = auth.getJWTToken();
 
-      return {
-        headers: token ? { authorization: `Bearer ${token}` } : undefined,
-      };
-    },
-  });
+            const headers = {
+              ...additionalHeaders,
+            };
+
+            if (token) {
+              headers['authorization'] = `Bearer ${token}`;
+            }
+            return {
+              headers,
+            };
+          },
+          reconnect: true,
+        }
+      )
+    : null;
+
+  const [dedupExchange, cacheExchange, fetchExchange] = defaultExchanges;
 
   return {
     client: createClient({
       url,
       exchanges: [
-        ...defaultExchanges,
+        dedupExchange,
+        cacheExchange,
+        // order of exchange is important, error exhange should come before fetchExchange
+        errorExchange({
+          onError(error, operation) {
+            if (errorHandler) {
+              errorHandler(error, operation);
+            }
+          },
+        }),
+        fetchExchange,
         subscriptionExchange({
-          forwardSubscription: (operation) => {
-            return {
-              subscribe: (sink) => ({
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                unsubscribe: subscriptionClient.subscribe(operation, sink),
-              }),
-            };
+          forwardSubscription(operation) {
+            return subscriptionClient.request(operation);
           },
         }),
       ],
       fetchOptions: () => {
         const token = auth.getJWTToken();
+
+        const headers = {
+          ...additionalHeaders,
+        };
+
+        if (token) {
+          headers['authorization'] = `Bearer ${token}`;
+        }
         return {
-          headers: token ? { authorization: `Bearer ${token}` } : undefined,
+          headers,
         };
       },
     }),
@@ -50,31 +82,35 @@ const createGQLClient = (url: string, auth: Auth) => {
   };
 };
 
-export interface GraphQLProviderProps {
+interface GraphQLProviderProps {
   children: React.ReactNode;
   auth: Auth;
   url: string;
+  errorHandler?: (arg1: any, arg2: any) => void;
+  additionalHeaders?: Record<string, unknown>;
 }
 
 export const GraphQLProvider = ({
   children,
   auth,
   url,
+  errorHandler,
+  additionalHeaders,
 }: GraphQLProviderProps) => {
-  const ref = useRef<{ client: Client; _sub: WSClient }>();
+  const ref = useRef<{ client: Client; _sub: SubscriptionClient }>();
   const { client } = ref.current || ({} as any);
   const [, render] = useState({});
   const { signedIn } = useAuth();
 
   useEffect(() => {
-    const r = createGQLClient(url, auth);
+    const r = createGQLClient(url, auth, errorHandler, additionalHeaders);
     ref.current = r;
     const { _sub } = r;
     render({});
     return () => {
-      _sub?.dispose();
+      _sub?.close();
     };
-  }, [signedIn, auth, url]);
+  }, [signedIn, auth, url, additionalHeaders]);
 
   if (!client) return null;
 
